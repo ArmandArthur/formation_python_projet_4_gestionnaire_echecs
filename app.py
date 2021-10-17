2c0123815ab93af07e7b7385aee6787a7b7730ef
Armand Arthur <armand.arthur@gmail.com>
2021-10-17 08:21:51 +0200

    json

--- app.py
+++ app.py
@@ -3,28 +3,67 @@
 
 
 from controllers.Player import PlayerController
-
 from views.QuestionsPlayerView import QuestionsPlayerView
+
+from pydantic import ValidationError
+
+import json
+import unicodedata
+from pathlib import Path
 
 class App:
     def __init__(self, controllerPlayer, QuestionsPlayer):
         self.controllerPlayer = controllerPlayer
         self.questionsPlayer = QuestionsPlayerView().main()
         self.answersPlayer = {}
+        self.pathPlayer = 'datas/players/'
         self.main()
         
     def main(self):
 
-        self.defQuestionsPlayer()
+        self.QuestionsPlayer()
 
-    def defQuestionsPlayer(self):
+    def QuestionsPlayer(self):
         for key, question in self.questionsPlayer.items():
             self.answersPlayer[key] = input(question)
         # New PLAYER
         newPlayer = self.answersPlayer
-        player = self.controllerPlayer(**newPlayer)
-        print(player)
+        self.VerifyPlayer(newPlayer)
+
+    def VerifyPlayer(self, newPlayer):
+        try:
+            player = self.controllerPlayer(**newPlayer)
+            self.InsertJsonPlayer(player)
+        except ValidationError as e:
+            print(e.json())
+
+    def InsertJsonPlayer(self, player):
+        dictPlayer = player.dict()
+
+        # default STR for date
+        # ensure ascii allow accents
+        jsonPlayerWithIndent = json.dumps(dictPlayer, indent=4, sort_keys=True, default=str, ensure_ascii=False)
         
+        # Remove accent
+        nameFile = self.strip_accents(player.Name+'_'+player.FirstName)
+
+        # Create FOLDER
+        Path(self.pathPlayer).mkdir(parents=True, exist_ok=True)
+
+        #Create FILE
+        with open(self.pathPlayer+nameFile+".json", "w") as outfile:
+            outfile.write(jsonPlayerWithIndent)
+
+    def strip_accents(self, text):
+        try:
+            text = unicode(text, 'utf-8')
+        except (TypeError, NameError): # unicode is a default on python 3 
+            pass
+        text = unicodedata.normalize('NFD', text)
+        text = text.encode('ascii', 'ignore')
+        text = text.decode("utf-8")
+        return str(text)
+
 if __name__ == "__main__":
     App(
         PlayerController,
